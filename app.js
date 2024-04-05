5795161f380852adec1c53d92322cabf0e7f2625
Juan Arede <85207414+juanarede@users.noreply.github.com>
2024-04-05 18:21:20 -0300

    middleware

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import router from './src/router/message.router.js';
+import { corseMiddleware } from './src/cors/middleware.js'
 
 export const createApp = () =>{
+
     const app = express();
     
     app.use(bodyParser.urlencoded({extended:true}))
 
     app.use(bodyParser.json())
     
-    app.use(cors());
+    app.use(corseMiddleware);
 
     app.use('/mail', router)
     
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { createApp } from "./app.js";
 
 
+
 createApp();
